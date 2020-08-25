85a8061573821927cb0376a33b8ac038e76aa7d3
Aswin Nair <aswintorch@gmail.com>
2020-08-25 17:27:16 -0400

    Initial Commit

---
+++ Torch News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Torch News
+//
+//  Created by Aswin Nair on 8/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Torch News/Torch_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Torch_NewsApp.swift
+//  Torch News
+//
+//  Created by Aswin Nair on 8/25/20.
+//
+
+import SwiftUI
+
+@main
+struct Torch_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
